55396a6077466c32286d42a041e6df389a77c87b
pierodesenzi <piero.desenzi@gmail.com>
2024-08-31 14:11:05 -0300

    Fixing local_invoker.py so that handler.py is reachable

--- src/local/local_invoker.py
+++ src/local/local_invoker.py
@@ -1,4 +1,11 @@
-from ..app.index import handler
+import sys
+from pathlib import Path
+
+# Add the root directory to sys.path
+root_dir = Path(__file__).resolve().parents[2]
+sys.path.append(str(root_dir))
+
+from src.app.index import handler
 
 # Calling the function
 amount_transferred = 100
